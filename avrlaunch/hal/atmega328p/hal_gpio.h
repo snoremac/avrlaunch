6fb8744b5c3743e504d194d901c17d2b46df9bdb
Cam Smith <cam@solidstate.com.au>
2014-03-15 10:39:27 +1100

    Added defs for the rest of the GPIO timers

--- avrlaunch/hal/atmega328p/hal_gpio.h
+++ avrlaunch/hal/atmega328p/hal_gpio.h
@@ -32,12 +32,12 @@ struct gpio_timer {
 })
 
 #define GPIO_TIMER(PORT, PIN) ((struct gpio_timer) { \
-	.timer_control = GPIO_TIMER_CONTROL_##PORTD_##PIN, \
-	.output_compare_low = GPIO_TIMER_OC_LOW_##PORTD_##PIN, \
-	.output_compare_high = GPIO_TIMER_OC_HIGH_##PORTD_##PIN, \
-	.output_compare_off = GPIO_TIMER_OC_OFF_##PORTD_##PIN, \
-	.output_compare_non_inverting = GPIO_TIMER_OC_NINV_##PORTD_##PIN, \
-	.output_compare_inverting = GPIO_TIMER_OC_INV_##PORTD_##PIN \
+	.timer_control = GPIO_TIMER_CONTROL_ ## PORT ## _ ## PIN, \
+	.output_compare_low = GPIO_TIMER_OC_LOW_ ## PORT ## _ ## PIN, \
+	.output_compare_high = GPIO_TIMER_OC_HIGH_ ## PORT ## _ ## PIN, \
+	.output_compare_off = GPIO_TIMER_OC_OFF_ ## PORT ## _ ## PIN, \
+	.output_compare_non_inverting = GPIO_TIMER_OC_NINV_ ## PORT ## _ ## PIN, \
+	.output_compare_inverting = GPIO_TIMER_OC_INV_ ## PORT ## _ ## PIN \
 })
 
 #define GPIO_INDEX_PORTB 0
@@ -55,6 +55,41 @@ struct gpio_timer {
 #define GPIO_DATA_PORTB &PORTB
 #define GPIO_DATA_PORTC &PORTC
 #define GPIO_DATA_PORTD &PORTD
+
+#define GPIO_TIMER_CONTROL_PORTB_PIN1 &TCCR1A
+#define GPIO_TIMER_OC_LOW_PORTB_PIN1 &OCR1AL
+#define GPIO_TIMER_OC_HIGH_PORTB_PIN1 &OCR1AH
+#define GPIO_TIMER_OC_OFF_PORTB_PIN1 (uint8_t) ~(1 << COM1A0 | 1 << COM1A1)
+#define GPIO_TIMER_OC_NINV_PORTB_PIN1 (1 << COM1A1)
+#define GPIO_TIMER_OC_INV_PORTB_PIN1 (1 << COM1A0 | 1 << COM1A1)
+
+#define GPIO_TIMER_CONTROL_PORTB_PIN2 &TCCR1A
+#define GPIO_TIMER_OC_LOW_PORTB_PIN2 &OCR1BL
+#define GPIO_TIMER_OC_HIGH_PORTB_PIN2 &OCR1BH
+#define GPIO_TIMER_OC_OFF_PORTB_PIN2 (uint8_t) ~(1 << COM1B0 | 1 << COM1B1)
+#define GPIO_TIMER_OC_NINV_PORTB_PIN2 (1 << COM1B1)
+#define GPIO_TIMER_OC_INV_PORTB_PIN2 (1 << COM1B0 | 1 << COM1B1)
+
+#define GPIO_TIMER_CONTROL_PORTB_PIN3 &TCCR2A
+#define GPIO_TIMER_OC_LOW_PORTB_PIN3 &OCR2A
+#define GPIO_TIMER_OC_HIGH_PORTB_PIN3 NULL
+#define GPIO_TIMER_OC_OFF_PORTB_PIN3 (uint8_t) ~(1 << COM2A0 | 1 << COM2A1)
+#define GPIO_TIMER_OC_NINV_PORTB_PIN3 (1 << COM2A1)
+#define GPIO_TIMER_OC_INV_PORTB_PIN3 (1 << COM2A0 | 1 << COM2A1)
+
+#define GPIO_TIMER_CONTROL_PORTD_PIN3 &TCCR2A
+#define GPIO_TIMER_OC_LOW_PORTD_PIN3 &OCR2B
+#define GPIO_TIMER_OC_HIGH_PORTD_PIN3 NULL
+#define GPIO_TIMER_OC_OFF_PORTD_PIN3 (uint8_t) ~(1 << COM2B0 | 1 << COM2B1)
+#define GPIO_TIMER_OC_NINV_PORTD_PIN3 (1 << COM2B1)
+#define GPIO_TIMER_OC_INV_PORTD_PIN3 (1 << COM2B0 | 1 << COM2B1)
+
+#define GPIO_TIMER_CONTROL_PORTD_PIN5 &TCCR0A
+#define GPIO_TIMER_OC_LOW_PORTD_PIN5 &OCR0B
+#define GPIO_TIMER_OC_HIGH_PORTD_PIN5 NULL
+#define GPIO_TIMER_OC_OFF_PORTD_PIN5 (uint8_t) ~(1 << COM0B0 | 1 << COM0B1)
+#define GPIO_TIMER_OC_NINV_PORTD_PIN5 (1 << COM0B1)
+#define GPIO_TIMER_OC_INV_PORTD_PIN5 (1 << COM0B0 | 1 << COM0B1)
 
 #define GPIO_TIMER_CONTROL_PORTD_PIN6 &TCCR0A
 #define GPIO_TIMER_OC_LOW_PORTD_PIN6 &OCR0A
