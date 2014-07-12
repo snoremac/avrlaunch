7e613ebef35db700e8d9d9eb17bec6b5b9a0d72c
Cam Smith <cam@solidstate.com.au>
2014-07-12 16:31:27 +1000

    Made system warning indicator work on the 32U4

--- avrlaunch/log.c
+++ avrlaunch/log.c
@@ -17,22 +17,42 @@ void LOG(const char* level, const char* format, ...) {
 }
 
 void set_system_warning() {
+#if defined (__AVR_ATmega328P__) || defined (__AVR_ATmega328__)
   gpio_set_output(&GPIO(PORTB, PIN5));
   gpio_write_high(&GPIO(PORTB, PIN5));
+#elif defined (__AVR_ATmega32U4__)
+  gpio_set_output(&GPIO(PORTC, PIN7));
+  gpio_write_high(&GPIO(PORTC, PIN7));
+#endif	
 }
 
 void clear_system_warning() {
+#if defined (__AVR_ATmega328P__) || defined (__AVR_ATmega328__)
   gpio_set_output(&GPIO(PORTB, PIN5));
   gpio_write_low(&GPIO(PORTB, PIN5));
+#elif defined (__AVR_ATmega32U4__)
+  gpio_set_output(&GPIO(PORTC, PIN7));
+  gpio_write_low(&GPIO(PORTC, PIN7));
+#endif	
 }
 
 void set_system_error() {
+#if defined (__AVR_ATmega328P__) || defined (__AVR_ATmega328__)
   gpio_set_output(&GPIO(PORTB, PIN5));
   gpio_write_high(&GPIO(PORTB, PIN5));
+#elif defined (__AVR_ATmega32U4__)
+  gpio_set_output(&GPIO(PORTC, PIN7));
+  gpio_write_high(&GPIO(PORTC, PIN7));
+#endif	
 }
 
 void clear_system_error() {
+#if defined (__AVR_ATmega328P__) || defined (__AVR_ATmega328__)
   gpio_set_output(&GPIO(PORTB, PIN5));
   gpio_write_low(&GPIO(PORTB, PIN5));
+#elif defined (__AVR_ATmega32U4__)
+  gpio_set_output(&GPIO(PORTC, PIN7));
+  gpio_write_low(&GPIO(PORTC, PIN7));
+#endif	
 }
 
